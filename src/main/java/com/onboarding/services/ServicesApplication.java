9272e59a1c278b9ad74a086a113ade096bd90ecf
Pranoti Shinde <prashind1@publicisgroupe.net>
2021-11-11 15:14:24 +0530

    cross origin changes

--- src/main/java/com/onboarding/services/ServicesApplication.java
+++ src/main/java/com/onboarding/services/ServicesApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 @ComponentScan("com.onboarding.services")
 @SpringBootApplication
 @EnableAutoConfiguration
--- src/main/java/com/onboarding/services/controller/OnBoardServiceController.java
+++ src/main/java/com/onboarding/services/controller/OnBoardServiceController.java
@@ -8,11 +8,9 @@ import com.onboarding.services.service.IOnBoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("onboard")
 public class OnBoardServiceController {
